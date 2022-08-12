e5124691f4d0aac21927fef7342974b71dca3682
Viktor <dmitrenko.v@ids-borjomi.com>
2022-08-12 14:45:46 +0700

    Updates

--- src/components/modal-overlay/modal-overlay.tsx
+++
@@ -1,8 +0,0 @@
-import style from './modal-overlay.module.css';
-
-export default function ModalOverlay({ onClose }: { onClose: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void }) {
-    return (
-        <div className={style.overlay} onClick={onClose}>
-        </div>
-    );
-}
--- src/services/hooks.ts
+++
@@ -1,9 +0,0 @@
-import {
-    TypedUseSelectorHook,
-    useDispatch as dispatchHook,
-    useSelector as selectorHook
-} from 'react-redux';
-import { AppDispatch, AppThunk, RootState } from '../utils/types';
-
-export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
-export const useDispatch: () => AppDispatch = dispatchHook;
